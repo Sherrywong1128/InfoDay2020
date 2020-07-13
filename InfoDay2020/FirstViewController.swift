93e99ca4299704f39e2d2ba389a4cdf5c93210cb
Sherrywong1128 <sherrywong1128@gmail.com>
2020-07-13 15:16:25 +0800

    Initial Commit

---
+++ InfoDay2020/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  InfoDay2020
+//
+//  Created by Tsztung Wong on 13/7/2020.
+//  Copyright © 2020 Tsztung Wong. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ InfoDay2020Tests/InfoDay2020Tests.swift
@@ -0,0 +1,34 @@
+//
+//  InfoDay2020Tests.swift
+//  InfoDay2020Tests
+//
+//  Created by Tsztung Wong on 13/7/2020.
+//  Copyright © 2020 Tsztung Wong. All rights reserved.
+//
+
+import XCTest
+@testable import InfoDay2020
+
+class InfoDay2020Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
